0e830ef18b32bf9d36a06bbb90e3d684672d71a2
Javier Gutierrez <javier.guse@icloud.com>
2020-11-01 13:39:34 -0300

    add update script to move files to the qmk folder

---
+++ update.sh
@@ -0,0 +1,14 @@
+function update_qmk_keyboard_files() {
+  local source_dir="./$1/"
+  local target_dir="$HOME/qmk_firmware/keyboards/$1/keymaps/javier-gs00" 
+  
+  if [[ -d $source_dir && -d $target_dir ]]; then
+    echo cp -r $source_dir $target_dir
+    cp -Rv $source_dir $target_dir
+    exit 0
+  else
+    exit 1
+  fi
+}
+
+update_qmk_keyboard_files $1
