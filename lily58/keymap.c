b7355b071bf3b1130c676c0e4ddfb391c089cb8a
Javier Gutierrez <javier.guse@icloud.com>
2020-11-08 18:10:06 -0300

    Swap Lower and LALT in the DEFAULT layer. Change the FUNC layer Add mod keys in the home row to the left split when not in the DEFAULT layer

--- lily58/keymap.c
+++ lily58/keymap.c
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  XXXXXXX,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                      KC_6,       KC_7,      KC_8,      KC_9,      KC_0,     XXXXXXX, \
-  KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                      KC_Y,       KC_U,      KC_I,      KC_O,      KC_P,     KC_BSPC, \
-  KC_ESC,      KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                      KC_H,       KC_J,      KC_K,      KC_L,      KC_SCLN,  KC_QUOT, \
-  MO(_LOWER),  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     TO(_GAMING),           XXXXXXX,  KC_N,       KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,  KC_ENT, \
-                                   XXXXXXX,  KC_LALT,  KC_LCTRL, KC_SPC,                KC_RSFT,  MO(_RAISE), KC_LGUI,   XXXXXXX \
+  XXXXXXX,     KC_1,     KC_2,     KC_3,     KC_4,       KC_5,                                   KC_6,       KC_7,      KC_8,      KC_9,      KC_0,     XXXXXXX, \
+  KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,       KC_T,                                   KC_Y,       KC_U,      KC_I,      KC_O,      KC_P,     KC_BSPC, \
+  KC_ESC,      KC_A,     KC_S,     KC_D,     KC_F,       KC_G,                                   KC_H,       KC_J,      KC_K,      KC_L,      KC_SCLN,  KC_QUOT, \
+  KC_LALT,     KC_Z,     KC_X,     KC_C,     KC_V,       KC_B,     TO(_GAMING),        XXXXXXX,  KC_N,       KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,  KC_ENT, \
+                                   XXXXXXX,  MO(_LOWER), KC_LCTRL, KC_SPC,             KC_RSFT,  MO(_RAISE), KC_LGUI,   XXXXXXX \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                                   _______,      _______, _______,_______, _______, _______,\
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   KC_BSLS,      KC_7,    KC_8,   KC_9,    KC_GRV,  _______, \
-  _______, XXXXXXX, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,                                   KC_LBRC,      KC_4,    KC_5,   KC_6,    KC_MINS, _______, \
-  _______, XXXXXXX, XXXXXXX, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX,                _______,  KC_RBRC,      KC_1,    KC_2,   KC_3,    KC_EQL,  _______, \
-                             _______, _______, _______, _______,                _______,  MO(_ADJUST),  KC_0,    _______\
+  _______, _______, _______, _______,  _______, _______,                                   _______,      _______, _______,_______, _______, _______,\
+  _______, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                                   KC_BSLS,      KC_7,    KC_8,   KC_9,    KC_GRV,  _______, \
+  _______, KC_LALT,  KC_LSFT, KC_LCTRL, KC_LGUI, XXXXXXX,                                   KC_LBRC,      KC_4,    KC_5,   KC_6,    KC_MINS, _______, \
+  _______, XXXXXXX, XXXXXXX, KC_CAPS,  XXXXXXX, XXXXXXX, XXXXXXX,                _______,  KC_RBRC,      KC_1,    KC_2,   KC_3,    KC_EQL,  _______, \
+                             _______,  _______, _______, _______,                KC_TAB,   MO(_FUNC),    KC_0,    _______\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -80,21 +80,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  _______,   _______, _______, _______, _______, _______,                                  _______, _______, _______, _______, _______, _______, \
-  _______,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                  KC_MSTP, KC_HOME, KC_PGUP, KC_VOLU, KC_MPRV, KC_DEL, \
-  _______,   XXXXXXX, KC_LSFT, XXXXXXX, KC_LGUI, XXXXXXX,                                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
-  MO(_FUNC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,               _______,  KC_MPLY, KC_END,  KC_PGDN, KC_VOLD, KC_MNXT, KC_MUTE, \
-                               _______, _______, _______, _______,               _______,  _______, _______, _______ \
+  _______,   _______, _______, _______,  _______,   _______,                                    _______, _______, _______, _______, _______, _______, \
+  _______,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,                                    KC_MSTP, KC_HOME, KC_PGUP, KC_VOLU, KC_MPRV, KC_DEL, \
+  _______,   KC_LALT, KC_LSFT, KC_LCTRL, KC_LGUI,   XXXXXXX,                                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
+  _______,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,               _______,  KC_MPLY, KC_END,  KC_PGDN, KC_VOLD, KC_MNXT, KC_MUTE, \
+                               _______,  MO(_FUNC), _______, _______,               _______,  _______, _______, _______ \
 ),
 /*
  * FUNCTION LAYER
  */
   [_FUNC] = LAYOUT( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, \
-  XXXXXXX, KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-                             _______, _______, _______, _______,          _______, _______, _______, _______ \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, KC_LALT, KC_LSFT, KC_LCTRL, KC_LGUI, XXXXXXX,                            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,\
+                             _______,  _______, _______, _______,          _______, _______, _______, _______ \
   ),
 /*
  * GAMING LAYER
@@ -103,8 +103,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                      KC_6,       KC_7,      KC_8,      KC_9,      KC_0,     XXXXXXX, \
   KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                      KC_Y,       KC_U,      KC_I,      KC_O,      KC_P,     KC_BSPC, \
   KC_LCTRL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                      KC_H,       KC_J,      KC_K,      KC_L,      KC_SCLN,  KC_QUOT, \
-  KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     TO(_QWERTY),           XXXXXXX,  KC_N,       KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,  KC_ENT, \
-                                 XXXXXXX,  KC_LALT,  KC_SPC,   KC_SPC,                KC_RSFT,  MO(_RAISE), KC_LGUI,   XXXXXXX \
+  KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,           TO(_QWERTY),  KC_N,       KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,  KC_ENT, \
+                                 KC_LALT,  KC_LCTRL, KC_SPC,   KC_SPC,                KC_RSFT,  MO(_RAISE), KC_LGUI,   XXXXXXX \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
